2f040b8fdc17ff5e152c57f66ec1c071645d83ee
Corey Carter <crc2231@gmail.com>
2020-08-05 10:51:31 -0600

    Add total_inventory test/method

--- lib/event.rb
+++ lib/event.rb
@@ -22,6 +22,25 @@ class Event
     @food_trucks.find_all {|truck| truck.inventory.include?(item)}
   end
 
+  def total_inventory
+    tot_inventory = Hash.new { |hash, key| hash[key] = {quantity: 0, food_trucks: []} }
+    @food_trucks.each do |truck|
+      truck.inventory.each do |item, amount|
+        tot_inventory[item][:quantity] += amount
+        tot_inventory[item][:food_trucks] << truck
+      end
+    end
+    tot_inventory
+  end
 
+  def overstocked_items
+    items = []
+    total_inventory.each_key do |item|
+      if total_inventory[item][:quantity] > 50 && total_inventory[item][:food_trucks].count > 1
+        items << item
+      end
+    end
+    items
+  end
 
 end
--- test/event_test.rb
+++ test/event_test.rb
@@ -30,6 +30,9 @@ class EventTest < Minitest::Test
     @food_truck3 = FoodTruck.new("Palisade Peach Shack")
 
     @food_truck3.stock(@item1, 65)
+
+    @food_truck3.stock(@item3, 10)
+
   end
 
   def test_it_exists
@@ -76,7 +79,35 @@ class EventTest < Minitest::Test
 
     assert_equal 345.00, @food_truck2.potential_revenue
 
-    assert_equal 243.75, @food_truck3.potential_revenue
+    assert_equal 296.75, @food_truck3.potential_revenue
   end
+
+  def test_total_inventory
+    skip
+    @event.add_food_truck(@food_truck1)
+
+    @event.add_food_truck(@food_truck2)
+
+    @event.add_food_truck(@food_truck3)
+
+    expected = {
+      @item1 => {quantity: 100, food_trucks: [@food_truck1, @food_truck3]},
+      @item2 => {quantity: 7, food_trucks: [@food_truck1]},
+      @item4 => {quantity: 50, food_trucks: [@foodtruck2]},
+      @item3 => {quantity: 35, food_trucks: [@food_truck2, @food_truck3]}
+                }
+
+    assert_equal expected, @event.total_inventory
+  end
+
+  # def test_overstocked_items
+  #   @event.add_food_truck(@food_truck1)
+  #
+  #   @event.add_food_truck(@food_truck2)
+  #
+  #   @event.add_food_truck(@food_truck3)
+  #
+  #   assert_equal [@item1], @event.overstocked_items
+  # end
 
 end
