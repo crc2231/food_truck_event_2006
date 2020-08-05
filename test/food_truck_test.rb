58fc1a226a69179ddd9c5c89921c014369bdf8d5
Corey Carter <crc2231@gmail.com>
2020-08-05 09:28:56 -0600

    Add inventory test

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -43,4 +43,13 @@ class FoodTruckTest < Minitest::Test
     assert_equal 55, @food_truck.check_stock(@item1)
   end
 
+  def test_it_shows_inventory
+    @food_truck.stock(@item1, 30)
+    @food_truck.stock(@item1, 25)
+    @food_truck.stock(@item2, 12)
+
+    expected = {@item1 => 55, @item2 => 12}
+  end
+
+
 end
