0d4f495114d7bc3c24245e2cf34d9ee2531ac186
Corey Carter <crc2231@gmail.com>
2020-08-05 09:42:31 -0600

    Add food_truck_names method/test

---
+++ lib/event.rb
@@ -0,0 +1,21 @@
+require './lib/food_truck'
+
+class Event
+  attr_reader :name, :food_trucks
+
+  def initialize(name)
+    @name = name
+    @food_trucks = []
+  end
+
+  def add_food_truck(truck)
+    @food_trucks << truck
+  end
+
+  def food_truck_names
+    @food_trucks.map do |food_truck|
+      food_truck.name
+    end
+  end
+
+end
