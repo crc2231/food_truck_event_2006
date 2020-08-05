739aeaf52760ac0498df2afa4a6b1edd5456c65b
Corey Carter <crc2231@gmail.com>
2020-08-05 09:59:17 -0600

    Add potential_revenue method

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -24,4 +24,12 @@ class FoodTruck
     end
   end
 
+  def potential_revenue
+    potential_revenue = 0
+    @inventory.each do |item, quantity|
+      potential_revenue += item.price * quantity
+    end
+    potential_revenue
+  end
+
 end
